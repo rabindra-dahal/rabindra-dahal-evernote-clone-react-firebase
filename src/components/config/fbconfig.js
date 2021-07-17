913ef4d25a0558bfaf75c165577159cf6f43390f
Rabindra <rabindra.dahal@ut.ee>
2021-07-18 00:00:37 +0300

    firebase configuration added to store note

---
+++ src/components/config/fbconfig.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+  var firebaseConfig = {
+    apiKey: "AIzaSyA8Z5kZcOo1WdGhdfq6yZ83rmuu7RZX3qQ",
+    authDomain: "evernote-clone-90c4f.firebaseapp.com",
+    projectId: "evernote-clone-90c4f",
+    storageBucket: "evernote-clone-90c4f.appspot.com",
+    messagingSenderId: "904381028587",
+    appId: "1:904381028587:web:e1f074e17130d71f7706ae"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
